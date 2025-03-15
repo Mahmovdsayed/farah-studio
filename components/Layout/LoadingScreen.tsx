374d3d0a57e55b3a86022e3fbf28c93fc33eba2a
Mahmovdsayed <mahmoudsayed3576@gmail.com>
2025-03-15 13:48:47 +0200

    loading

--- components/Layout/LoadingScreen.tsx
+++ components/Layout/LoadingScreen.tsx
@@ -25,28 +25,6 @@ const LoadingScreen = () => {
             >
                 powered by NEST
             </motion.h4>
-
-            <motion.div
-                className="mt-8"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{
-                    delay: 0.6,
-                    duration: 0.5,
-                }}
-            >
-                <motion.div
-                    className="w-16 h-16 border-4 border-t-4 border-t-blue-500 border-gray-300 rounded-full"
-                    animate={{
-                        rotate: [0, 360],
-                    }}
-                    transition={{
-                        repeat: Infinity,
-                        duration: 1.5,
-                        ease: "linear",
-                    }}
-                />
-            </motion.div>
         </div>
     </>;
 };
