0c75f78dc139d0256498c70e22a11413a7eef816
Mahmovdsayed <mahmoudsayed3576@gmail.com>
2025-03-17 15:22:33 +0200

    grid

--- components/Sections/Projects.tsx
+++ components/Sections/Projects.tsx
@@ -94,7 +94,7 @@ const Projects = ({ data }: { data: any }) => {
                         </div>
                     </div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                         {sortedReels.map((rel, index) => (
                             <motion.div
                                 key={index}
